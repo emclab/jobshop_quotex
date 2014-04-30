cebd00f40eb2f2785060be7743686cdf17eea853
jc <emclab2011@gmail.com>
2014-04-30 16:09:03 -0500

    JC: added return customer by access right to app controller and customer_class to /lib and init file. spec passes.

--- lib/jobshop_quotex.rb
+++ lib/jobshop_quotex.rb
@@ -1,7 +1,7 @@
 require "jobshop_quotex/engine"
 
 module JobshopQuotex
-  mattr_accessor :quote_task_class, :mfg_process_class, :rfq_class, :new_order_path, :order_resource
+  mattr_accessor :quote_task_class, :mfg_process_class, :rfq_class, :new_order_path, :order_resource, :customer_class
   
   def self.quote_task_class
     @@quote_task_class.constantize
@@ -13,5 +13,9 @@ module JobshopQuotex
   
   def self.rfq_class
     @@rfq_class.constantize
+  end
+  
+  def self.customer_class
+    @@customer_class.constantize
   end
 end
